064fc429a149709edef95a747c8e3c5bed142393
Chaeunnie <guilbertchristianeledia@gmail.com>
2026-07-13 01:28:12 +0800

    Update route.ts

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,10 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const API_URL =
+  process.env.API_URL ??
+  "http://127.0.0.1:8000";
+
 export async function POST(request: NextRequest) {
   const formData = await request.formData();
 
   const response = await fetch(
-    "http://127.0.0.1:8000/upload/",
+    `${API_URL}/upload/`,
     {
       method: "POST",
       body: formData,
